413b4e27cf83d18076d947ddad8bc4a768384df5
Vito Vittore <vito.vittore@gmail.com>
2021-06-28 21:30:46 -0500

    adding meeting page

--- assets/js/home/index.js
+++ assets/js/home/index.js
@@ -1,1 +1,8 @@
 import '../../css/style.scss';
+import { v4 as uuidv4 } from 'uuid';
+
+const joinRoomBtn = document.querySelector('#join-room-btn');
+
+joinRoomBtn.addEventListener('click', () => {
+  location.assign(`/room.html?roomId=${uuidv4()}`);
+});
