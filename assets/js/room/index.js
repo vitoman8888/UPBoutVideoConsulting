b6cfb0f6577dd205ef1730629f1aaab4aba5b3c9
Vito Vittore <vito.vittore@gmail.com>
2021-06-30 20:11:51 -0500

    Completed Unit 4.2 of exercise

--- assets/js/room/index.js
+++ assets/js/room/index.js
@@ -1,9 +1,16 @@
 import '../../css/style.scss';
+import { renderFilterOptions, handleUpdateFilter, setCanvas } from './canvas';
 
 const roomIdEl = document.querySelector('#room-id');
 const clipboardBtn = document.querySelector('#clipboard-btn');
+const localVideoEl = document.querySelector('#local-video');
+const remoteVideoEl = document.querySelector('#remote-video');
+const localCanvasEl = document.querySelector('#local-canvas');
+const remoteCanvasEl = document.querySelector('#remote-canvas');
 
 let roomId = null;
+let stream = null;
+const filterOptionsSelectEl = document.querySelector('#filter-options');
 
 const getQueryStringParams = (query) => {
   return query
@@ -39,5 +46,39 @@ const copyToClipboard = async () => {
       console.error('Failed to copy!', err);
     }
   };
+
+  const startVideo = async () => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({
+        video: {
+          width: { min: 640, ideal: 1920 },
+          height: { min: 400, ideal: 1080 },
+          aspectRatio: { ideal: 1.7777777778 },
+        },
+        audio: true,
+      });
+      localVideoEl.srcObject = mediaStream;
+      stream = mediaStream;
+      setCanvas(localCanvasEl, localVideoEl);
   
+      return mediaStream;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  const handleSelectChange = (event) => {
+    handleUpdateFilter(event.target.value);
+  };
+
+  startVideo();
+  // renderCanvas(localCanvasEl);
+  renderFilterOptions(filterOptionsSelectEl);
+  
+  filterOptionsSelectEl.addEventListener('change', handleSelectChange);
+
   clipboardBtn.addEventListener('click', copyToClipboard);
+
+
+
+  
